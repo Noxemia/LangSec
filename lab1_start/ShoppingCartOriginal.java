06ab9a644cd642ea1a28d1d605a8aa1ec35adb9a
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2022-04-08 20:37:07 +0200

    Add original ShoppingCart

---
+++ lab1_start/ShoppingCartOriginal.java
@@ -0,0 +1,37 @@
+import backEnd.*;
+import java.util.Scanner;
+
+public class ShoppingCartOriginal {
+    private static void print(Wallet wallet, Pocket pocket) throws Exception {
+        System.out.println("Your current balance is: " + wallet.getBalance() + " credits.");
+        System.out.println(Store.asString());
+        System.out.println("Your current pocket is:\n" + pocket.getPocket());
+    }
+
+    private static String scan(Scanner scanner) throws Exception {
+        System.out.print("What do you want to buy? (type quit to stop) ");
+        return scanner.nextLine();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Wallet wallet = new Wallet();
+        Pocket pocket = new Pocket();
+        Scanner scanner = new Scanner(System.in);
+
+        print(wallet, pocket);
+        String product = scan(scanner);
+
+        while(!product.equals("quit")) {
+            if(Store.getProductPrice(product) <= wallet.getBalance()){
+                wallet.setBalance(wallet.getBalance() - Store.getProductPrice(product));
+                pocket.addProduct(product);
+            }else{
+                System.out.println("Not enough cash yo");
+                System.exit(0);
+            }
+
+            print(wallet, pocket);
+            product = scan(scanner);
+        }
+    }
+}
